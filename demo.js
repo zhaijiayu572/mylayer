05aad51e90f3cb418ea5819e9421f93bbbc72e85
zhaijiayu572 <zjypersonal@163.com>
2017-04-08 22:22:57 +0800

    完成简单的样式，和关闭效果

--- demo.js
+++ demo.js
@@ -8,10 +8,21 @@ function Layer() {
         };
         $.extend(option,config);
         $myAlert = $('<div class="layer-alert"></div>');
-        $myTitle = $('<h3 class="layer-alert-top"><span class="layer-alert-title">'+option.title+'</span><span class="layer-alert-close">X</span></h3>');
+        $myTitle = $('<h3 class="layer-alert-top"><span class="layer-alert-title">'+option.title+'</span></h3>');
+        $closeBtn = $('<span class="layer-alert-close">X</span>');
         $myContent = $('<p class="layer-alert-content">'+option.content+'</p>');
+        $confirmBtn = $('<button class="confirm-btn">确定</button>');
+        $confirmBtn.on('click',function () {
+            $myAlert.remove();
+        });
+        $closeBtn.on('click',function () {
+           $myAlert.remove();
+        });
+        $closeBtn.appendTo($myTitle);
+        $confirmBtn.appendTo($myAlert);
         $myTitle.appendTo($myAlert);
         $myContent.appendTo($myAlert);
         $myAlert.css({height:option.height,width:option.width}).appendTo($('body'));
     }
 }
+window.layer = new Layer();
