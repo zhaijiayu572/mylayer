e3817aaade1b0ba1d84f458434bde6ddacc1a209
zhaijiayu572 <zjypersonal@163.com>
2017-04-11 21:42:53 +0800

    增加了open方法

--- demo.js
+++ demo.js
@@ -23,6 +23,29 @@ function Layer() {
         $myTitle.appendTo($myAlert);
         $myContent.appendTo($myAlert);
         $myAlert.css({height:option.height,width:option.width}).appendTo($('body'));
+    };
+    this.open = function (config) {
+          var option = {
+              title:'提示', //如需关闭则传入false
+              content:null,  //请传入你需要包裹在弹窗里面的jQ对象或者一个字符串
+          };
+          $.extend(opotion,config);
+          var $openContainer = $("<div class='layer-open-container'></div>");
+          var $closeBtn = $('<span class="layer-open close-btn">X</span>');
+          var $top = $('<div class="layer-open-top"></div>');
+          if(!option.title){
+              $title = $('<div class="open-title">'+option.title+'</div>');
+              $title.appendTo($top);
+          }
+          $closeBtn.appendTo($top);
+          var $body = $('<div class="layer-open-body"></div>');
+          if(typeof(option.content)==='object'){
+              option.content.appendTo($body);
+          }else{
+              var $content = $('<p class="open-content">'+option.content+'</p>');
+              $content.appendTo($body);
+          }
+
     }
 }
 window.layer = new Layer();
